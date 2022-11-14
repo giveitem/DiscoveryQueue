b3c9c3a4d8947424ceb7c76655e8ce6128f6f6c3
Deshi Zhang <zdspeace@gmail.com>
2022-11-14 01:29:27 -0800

    server

--- server/routes.js
+++ server/routes.js
@@ -20,12 +20,42 @@ connection.connect();
 // Route 1 (handler)
 async function hello(req, res) {
     // a GET request to /hello
-    res.send(`Hello! Welcome to the FIFA server!`)
+    connection.query(`SELECT Name, Id FROM Tracks where danceability >0.32 and danceability < 0.33 LIMIT 10`, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    });
 
 }
 
 
+async function search(req, res) {
+    let base = "SELECT name, id FROM Tracks WHERE name LIKE '%" + req.query.name + "%'";
+    let page = req.query.page ? req.query.page : 1;
+    let pagesize = req.query.pagesize ? req.query.pagesize : 10;
+    let offset = (page - 1) * pagesize;
+    let limit = " LIMIT " + pagesize + " OFFSET " + offset;
+    let query = base + limit;
+    console.log(query);
+    connection.query(query, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            res.json({ error: error })
+        } else if (results) {
+            res.json({ results: results })
+        }
+    });
+}
+
+async function random(req, res) {
+
+}
+
 
 module.exports = {
-    hello
+    hello,
+    search
 }
