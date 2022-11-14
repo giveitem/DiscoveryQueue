bedd4fe70cca29956cb02eb2020eea3ac9a3454b
Deshi Zhang <zdspeace@gmail.com>
2022-11-14 01:27:46 -0800

    11/14

---
+++ client/src/fetcher.js
@@ -0,0 +1,29 @@
+import config from './config.json'
+
+const getSearchSongs = async (name) => {
+    var res = await fetch(`http://${config.server_host}:${config.server_port}/search?name=${name}`, {
+        method: 'GET',
+    })
+    var ans = await res.json()
+    console.log(ans)
+    return ans;
+}
+const getRandSongs = async () => {
+
+
+    var attrs = ["valence", "danceability", "energy", "key", "tempo"]
+    var resultArray = []
+    attrs.forEach(attr => {
+        var highRes = await fetch(`http://${config.server_host}:${config.server_port}/random?attr=${attr}&endpoint=high`, {
+            method: 'GET',
+        });
+        var lowRes = await fetch(`http://${config.server_host}:${config.server_port}/random?attr=${attr}&endpoint=low`, {
+            method: 'GET',
+        });
+        var highRes = await highRes.json();
+        var lowRes = await lowRes.json();
+        resultArray.push((highRes, lowRes));
+    })
+    return resultArray;
+}
+export { getSearchSongs }
--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 
 // Route 1 - register as GET 
-app.get('/hello', routes.hello)
-
+app.get('/', routes.hello);
+app.get('/search', routes.search);
 
 
 app.listen(config.server_port, () => {
